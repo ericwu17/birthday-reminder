c4c8ab811b46e11c0d53e6e577f62c045cf69a9a
Eric <ericwu17@protonmail.com>
2025-07-20 12:32:58 -0700

    handle send email error (by panicking)

--- src/test_email.rs
+++ src/test_email.rs
@@ -6,7 +6,8 @@ pub fn test_email() {
     send_email(
         "Test email",
         "This test email is sent from Birthday Reminders",
-    );
+    )
+    .expect("Error sending email");
 
     println!("Test email has been sent");
 }
